04727327eb36b7ae1e6ee051c5f741d9273e25a3
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2021-01-27 23:03:55 -0500

    CharacterController code from other project

--- Racoon/Assets/Scripts/CharacterController.cs
+++ Racoon/Assets/Scripts/CharacterController.cs
@@ -2,34 +2,112 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : KinematicObject
 {
-    public Rigidbody rb;
-    public float force = 10f;
-    public float walkForce = 0.5f;
 
-    void Start()
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    public JumpState jumpState = JumpState.Grounded;
+    private bool stopJump;
+    public Collider2D collider2d;
+    public bool controlEnabled = true;
+
+    bool jump;
+    Vector2 move;
+    SpriteRenderer spriteRenderer;
+    public float jumpModifier = 1.5f;
+    public float jumpDeceleration = 0.5f;
+
+    public Bounds Bounds => collider2d.bounds;
+
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        collider2d = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-
-    void Update()
+    protected override void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (controlEnabled)
         {
-            Jump();
+            move.x = Input.GetAxis("Horizontal");
+            if (jumpState == JumpState.Grounded && Input.GetButtonDown("Jump"))
+                jumpState = JumpState.PrepareToJump;
+            else if (Input.GetButtonUp("Jump"))
+            {
+                stopJump = true;
+                //Schedule<PlayerStopJump>().player = this;
+            }
         }
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        else
         {
-            rb.AddForce(Vector3.right * walkForce, ForceMode.Acceleration);
+            move.x = 0;
+        }
+        UpdateJumpState();
+        base.Update();
+    }
+
+    void UpdateJumpState()
+    {
+        jump = false;
+        switch (jumpState)
+        {
+            case JumpState.PrepareToJump:
+                jumpState = JumpState.Jumping;
+                jump = true;
+                stopJump = false;
+                break;
+            case JumpState.Jumping:
+                if (!IsGrounded)
+                {
+                  //  Schedule<PlayerJumped>().player = this;
+                    jumpState = JumpState.InFlight;
+                }
+                break;
+            case JumpState.InFlight:
+                if (IsGrounded)
+                {
+                   // Schedule<PlayerLanded>().player = this;
+                    jumpState = JumpState.Landed;
+                }
+                break;
+            case JumpState.Landed:
+                jumpState = JumpState.Grounded;
+                break;
         }
     }
 
-    public void Jump()
+    protected override void ComputeVelocity()
     {
-        rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+        if (jump && IsGrounded)
+        {
+            velocity.y = jumpTakeOffSpeed * jumpModifier;
+            jump = false;
+        }
+        else if (stopJump)
+        {
+            stopJump = false;
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * jumpDeceleration;
+            }
+        }
+
+        if (move.x > 0.01f)
+            spriteRenderer.flipX = false;
+        else if (move.x < -0.01f)
+            spriteRenderer.flipX = true;
+
+        targetVelocity = move * maxSpeed;
     }
 
-    
+    public enum JumpState
+    {
+        Grounded,
+        PrepareToJump,
+        Jumping,
+        InFlight,
+        Landed
+    }
 }
