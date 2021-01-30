b2b513c0685346b8c03d7607ecc0e5806220a04f
Nicaudio <78127273+Nicaudio@users.noreply.github.com>
2021-01-30 01:05:37 -0500

    Post Movement script audio added to Prefab

---
+++ Racoon/Assets/Scripts/Audio/PostMovementEvents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostMovementEvents : MonoBehaviour
+{
+    // Name the events
+    public AK.Wwise.Event FootstepEvent;
+    public AK.Wwise.Event FoleyEvent;
+
+    // Post the footstep event
+
+    public void PlayFootstepSound()
+    {
+        FootstepEvent.Post(gameObject);
+    }
+
+    // Post the foley event
+    public void PlayFoleySound()
+    {
+        FoleyEvent.Post(gameObject);
+    }
+}
