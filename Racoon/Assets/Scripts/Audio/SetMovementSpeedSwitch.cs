1171d63e50d2de28dca552c81faffb7dea821486
Nicaudio <78127273+Nicaudio@users.noreply.github.com>
2021-01-30 01:53:22 -0500

    Last changes
    Impossible de mettre RVBs dans environnement 2D avec scripts standards. User Game defined Aux Send

---
+++ Racoon/Assets/Scripts/Audio/SetMovementSpeedSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMovementSpeedSwitch : MonoBehaviour
+{
+    // Name the switches
+    public AK.Wwise.Switch MovementTypeJump;
+    public AK.Wwise.Switch MovementTypeRun;
+    public AK.Wwise.Switch MovementTypeLand;
+
+    // This gets voided when the Function SetJumpSwitch() is called in an animation event, it sets the switch value in Wwise
+    public void SetJumpSwitch()
+    {
+        MovementTypeJump.SetValue(gameObject);
+    }
+
+    // This gets voided when the Function SetRunSwitch() is called in an animation event, it sets the switch value in Wwise
+    public void SetRunSwitch()
+    {
+        MovementTypeRun.SetValue(gameObject);
+    }
+
+    // This gets voided when the Function SetLandSwitch() is called in an animation event, it sets the switch value in Wwise
+    public void SetLandSwitch()
+    {
+        MovementTypeLand.SetValue(gameObject);
+    }
+
+}
