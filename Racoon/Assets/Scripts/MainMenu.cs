2f7cd937d9e9713ddd9b1146d4c23c095a3d42a7
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2021-01-31 14:54:07 -0500

    Ui sounds

--- Racoon/Assets/Scripts/MainMenu.cs
+++ Racoon/Assets/Scripts/MainMenu.cs
@@ -7,11 +7,26 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 
+    public AK.Wwise.Event uiBack;
+    public AK.Wwise.Event uiHover;
+    public AK.Wwise.Event uiSelect;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-
+    public void PlayUISelect()
+    {
+        uiSelect.Post(gameObject);
+    }
+    public void PlayUIHover()
+    {
+        uiHover.Post(gameObject);
+    }
+    public void PlayUIBack()
+    {
+        uiBack.Post(gameObject);
+    }
 
 }
